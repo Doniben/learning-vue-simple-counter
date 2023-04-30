5920fb73775902d5da0393f13ed75db28bb58817
Doniben <doniben@esperanto.co>
2023-04-29 23:39:32 -0600

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,17 @@ yarn build
 # navigate into the build output directory
 cd dist
 
-git init
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init -b main
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:doniben/learning-vue-simple-counter.git master:gh-pages
+git push -f git@github.com:doniben/learning-vue-simple-counter.git main:gh-pages
 
 cd -
